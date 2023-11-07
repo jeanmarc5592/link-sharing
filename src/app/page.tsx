6834a24dc12badbd1fc7e67e0da4e5d28552babb
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-07 10:19:26 +0100

    Remove test elements from Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,9 @@
-"use client"
-
-import { useState } from "react";
-import Button from "./_modules/common/components/Button";
-import Dropdown from "./_modules/common/components/Dropdown";
-import Input from "./_modules/common/components/Input";
-import Typography from "./_modules/common/components/Typography";
-import { trpc } from "./_trpc/client"
-import { PLATFORMS } from "@/lib/constants/platforms";
-
-const Icon = () => {
+const HomePage = () => {
   return (
-    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-        <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
-        <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
-    </svg>
-  )
-}
-
-export default function Home() {
-  const { data: users } = trpc.getUsers.useQuery();
-  const [platform, setPlatform] = useState(PLATFORMS[0]);
-
-  return (
-    <main className="p-4">
-      <pre>{JSON.stringify(users, null, 2)}</pre>
-      <br />
-
-      <p className="text-custom-purple">Custom Purple</p>
-      <p className="text-custom-purple-lighter">Custom Purple Lighter</p>
-      <p className="text-custom-purple-light">Custom Purple Light</p>
-      <p className="text-custom-black">Custom Black</p>
-      <p className="text-custom-gray">Custom Gray</p>
-      <p className="text-custom-gray-lighter">Custom Gray Lighter</p>
-      <p className="text-custom-gray-light">Custom Gray Light</p>
-      <p className="text-custom-red">Custom Gray Red</p>
-      <br />
-
-      <Typography variant="Heading M">Heading M</Typography>
-      <Typography variant="Heading S">Heading S</Typography>
-      <Typography variant="Body M">Body M</Typography>
-      <Typography variant="Body S">Body S</Typography>
-      <br />
-
-      <Input label="This is the label" inputProps={{ placeholder: "A cool placeholder" }} icon={<Icon />} />
-      <Input label="This is the label" inputProps={{ placeholder: "A cool placeholder" }} error="Error Message" icon={<Icon />} />
-      <br />
-
-      <Button onClick={() => console.log("click")}>Button</Button>
-      <Button onClick={() => console.log("click")} disabled>Button</Button>
-      <br />
-
-      <Button onClick={() => console.log("click")} variant="secondary">Button</Button>
-      <Button onClick={() => console.log("click")} variant="secondary" disabled>Button</Button>
-      <br />
-
-      <Dropdown data={PLATFORMS} selected={platform} setSelected={setPlatform} />
+    <main>
+      HOME PAGE
     </main>
   )
 }
+
+export default HomePage;
