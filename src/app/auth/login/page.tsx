89cc854ce5673dc2da357350a75f4220b5261fdc
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-07 10:18:49 +0100

    Add Auth Pages

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "@/app/_modules/auth/components/LoginForm"
+
+const LoginPage = () => {
+  return (
+    <main className="w-[100vw] h-[100vh] flex justify-center items-center">
+      {/* LOGO */}
+
+      <LoginForm />
+    </main>
+  )
+}
+
+export default LoginPage
---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+const SignupPage = () => {
+  return (
+    <div>
+      SIGNUP PAGE
+    </div>
+  )
+}
+
+export default SignupPage
