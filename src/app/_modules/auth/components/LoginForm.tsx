bbdc71ce849efc14aa3a56922d6ed83066b7cb21
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-07 10:44:42 +0100

    Slice Auth components to make them reusable

--- src/app/_modules/auth/components/LoginForm.tsx
+++ src/app/_modules/auth/components/LoginForm.tsx
@@ -3,30 +3,31 @@
 import Button from "../../common/components/Button"
 import Input from "../../common/components/Input"
 import Typography from "../../common/components/Typography"
+import AuthCard from "./AuthCard"
 
 const LoginForm = () => {
 
   return (
-    <div className="bg-white p-10 rounded-md shadow-sm">
-        <Typography variant="Heading M" className="mb-4">Login</Typography>
-        <Typography className="mb-12">Add your details below to get back into the app</Typography>
+    <AuthCard>
+      <Typography variant="Heading M" className="mb-4">Login</Typography>
+      <Typography className="mb-10 sm:mb-12">Add your details below to get back into the app</Typography>
 
-        <Input 
-          label="Email address" 
-          inputProps={{ placeholder: "e.g alex@email.com" }}
-        />
+      <Input 
+        label="Email address" 
+        inputProps={{ placeholder: "e.g alex@email.com" }}
+      />
 
-        <Input 
-          label="Password" 
-          inputProps={{ placeholder: "Enter your password", type: "password" }}
-        />
+      <Input 
+        label="Password" 
+        inputProps={{ placeholder: "Enter your password", type: "password" }}
+      />
 
-        <Button>Login</Button>
+      <Button>Login</Button>
 
-        <Typography className="text-center">
-          Don&apos;t have an account? <a className="text-custom-purple" href="/auth/signup">Create account</a>
-        </Typography>
-      </div>
+      <Typography className="text-center">
+        Don&apos;t have an account? <a className="text-custom-purple" href="/auth/signup">Create account</a>
+      </Typography>
+    </AuthCard>
   )
 }
 
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,9 +2,7 @@ import LoginForm from "@/app/_modules/auth/components/LoginForm"
 
 const LoginPage = () => {
   return (
-    <main className="w-[100vw] h-[100vh] flex justify-center items-center">
-      {/* LOGO */}
-
+    <main className="w-[100vw] h-[100vh] flex justify-center sm:items-center">
       <LoginForm />
     </main>
   )
