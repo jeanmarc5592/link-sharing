b693c0e5ca7aa5cbc635f1858f231b66fc69ed7e
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-10 13:54:46 +0100

    Add logging to "findByEmail"

--- src/server/services/users.ts
+++ src/server/services/users.ts
@@ -7,6 +7,12 @@ export class UsersService {
       const user = await prisma.user.findUnique({
         where: { email }
       });
+     
+      if (!user) {
+        console.log(`User with email "${email}" not found.`)
+        return null;
+      }
+
       return user;
     } catch (error) {
       console.error(error);
