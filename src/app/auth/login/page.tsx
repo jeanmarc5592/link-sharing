f56be5c0360c46ff93449c9152823050696e4926
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-10 10:24:46 +0100

    Add redirects to auth pages

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,15 @@
 import LoginForm from "@/app/_modules/auth/components/LoginForm"
+import { authOptions } from "@/lib/auth/authOptions"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <main className="w-[100vw] h-[100vh] flex justify-center sm:items-center">
       <LoginForm />
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,6 +1,15 @@
+import { authOptions } from '@/lib/auth/authOptions';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import SignupForm from '../../_modules/auth/components/SignupForm';
 
-const SignupPage = () => {
+const SignupPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center sm:items-center">
       <SignupForm />
