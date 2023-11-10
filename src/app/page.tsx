af2683e8338465ad2103b9bfe7bd4609b93e4bb8
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-10 09:15:01 +0100

    Add logout functionality for testing purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client"
+
+import { signOut } from "next-auth/react";
+import Button from "./_modules/common/components/Button";
+
 const HomePage = () => {
+  // TODO: Use AuthService here
+  const logout = () =>  {
+    signOut({ callbackUrl: "/auth/login" });
+  };
+
   return (
     <main>
       HOME PAGE
+      
+      <div className="w-[25vw]">
+        <Button variant="primary" onClick={logout}>Log out</Button>
+      </div>
     </main>
   )
 }
