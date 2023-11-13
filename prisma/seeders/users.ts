7a92fca3ba356342b11487e2d6c7e27a4d5d656e
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-13 17:55:53 +0100

    Create UsersSeeder

---
+++ prisma/seeders/users.ts
@@ -0,0 +1,17 @@
+import { PrismaClient, User } from "@prisma/client";
+import { SeederInterface } from "../types";
+
+export class UsersSeeder implements SeederInterface {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async generateOne(): Promise<User> {
+    return await this.prisma.user.upsert({
+      where: { email: "user@email.com" },
+      update: {},
+      create: {
+        email: "user@email.com",
+        password: "$2a$12$XGxkVDB5AduU5nt1SaPAwe6X0.Nb4.UEd3i26fokZzQIE.eXwTlgG", // password
+     }
+    });
+  }
+}
