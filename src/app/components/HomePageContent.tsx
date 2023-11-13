21bbd0618ba424c2bcf0ff2a4b7299bf88a842f0
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-13 12:59:27 +0100

    Create Basic Layout for page content

--- src/app/components/HomePageContent.tsx
+++ src/app/components/HomePageContent.tsx
@@ -1,13 +1,21 @@
 "use client"
 
 import { useAppSelector } from "../common/hooks/useAppSelector"
+import Links from "./Links";
+import Profile from "./Profile";
 
 const HomePageContent = () => {
   const activeTab = useAppSelector((state) => state.homeTabs.activeTab);
 
   return (
-    <div>
-      Active Tab: {activeTab}
+    <div className="flex w-full px-4">
+      <div className="bg-white rounded-md w-1/3 mr-4 p-6">
+        PREVIEW
+      </div>
+      <div className="bg-white w-2/3 rounded-md p-6">
+        {activeTab === "links" && <Links />}
+        {activeTab === "profile" && <Profile />}
+      </div>
     </div>
   )
 }
---
+++ src/app/components/Links.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LinkCard = () => {
+  return (
+    <div>
+      Links
+    </div>
+  )
+}
+
+export default LinkCard
