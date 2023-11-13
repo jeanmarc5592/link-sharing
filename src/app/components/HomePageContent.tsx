e164528dbd599d4488613bb0b423aadcd1847d21
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-13 17:25:35 +0100

    Add Phone Mockup

--- src/app/components/HomePageContent.tsx
+++ src/app/components/HomePageContent.tsx
@@ -10,7 +10,7 @@ const HomePageContent = () => {
 
   return (
     <div className="flex w-full px-4">
-      <div className="bg-white rounded-md w-1/3 mr-4 p-6 hidden lg:block">
+      <div className="bg-white rounded-md w-1/3 mr-4 p-6 hidden lg:flex justify-center items-center">
         <Preview />
       </div>
       <div className="bg-white rounded-md w-full lg:w-2/3 p-6">
--- src/app/components/Preview.tsx
+++ src/app/components/Preview.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import PhoneMockup from '../common/components/icons/PhoneMockup'
 
 const Preview = () => {
   return (
-    <div>
-      Preview
-    </div>
+    <>
+      <PhoneMockup />
+    </>
   )
 }
 
