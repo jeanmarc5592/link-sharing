5b4b21261857348c9ab0a2937f40d01ec985d3ec
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-15 09:19:27 +0100

    Add mb-4

--- src/app/common/components/Dropdown.tsx
+++ src/app/common/components/Dropdown.tsx
@@ -14,63 +14,65 @@ interface DropdownProps {
 
 const Dropdown: React.FC<DropdownProps> = ({ label, data, selected, setSelected }) => {
   return (
-    <Listbox value={selected} onChange={setSelected}>
-      {({ open }) => (
-        <>
-          <Listbox.Label className="block text-custom-gray">{label}</Listbox.Label>
-          <div className="relative mt-2">
-            <Listbox.Button className="relative w-full cursor-pointer bg-white py-2.5 pl-3 pr-10 text-left text-custom-black border border-custom-gray-lighter custom-black rounded-md focus:border-custom-purple">
-              <span className="flex items-center">
-                <span className="block truncate">{StringUtils.capitalize(selected.id)}</span>
-                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                  <ChevronUpDownIcon className="h-5 w-5 text-custom-gray" aria-hidden="true" />
+    <div className="mb-4">
+        <Listbox value={selected} onChange={setSelected}>
+        {({ open }) => (
+          <>
+            <Listbox.Label className="block text-custom-gray">{label}</Listbox.Label>
+            <div className="relative mt-2">
+              <Listbox.Button className="relative w-full cursor-pointer bg-white py-2.5 pl-3 pr-10 text-left text-custom-black border border-custom-gray-lighter custom-black rounded-md focus:border-custom-purple">
+                <span className="flex items-center">
+                  <span className="block truncate">{StringUtils.capitalize(selected.id)}</span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                    <ChevronUpDownIcon className="h-5 w-5 text-custom-gray" aria-hidden="true" />
+                  </span>
                 </span>
-              </span>
-            </Listbox.Button>
+              </Listbox.Button>
 
-            <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Listbox.Options className="absolute cursor-pointer z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-custom-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {data.map((element) => (
-                  <Listbox.Option
-                    key={element.id}
-                    className="relative cursor-default select-none py-2 pl-3 pr-9"
-                    value={element}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center cursor-pointer">
-                          <span
-                            className={clsx(selected ? 'text-custom-purple' : 'text-custom-black', 'block truncate')}
-                          >
-                            {StringUtils.capitalize(element.id)} {selected && "(Selected)"}
-                          </span>
-                          {selected ? (
-                          <span
-                            className={clsx(
-                              active ? 'text-white' : 'text-custom-purple',
-                              'absolute inset-y-0 right-0 flex items-center pr-4'
-                            )}
-                          >
-                            <></>
-                          </span>
-                        ) : null}
-                        </div>
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </>
-      )}
-    </Listbox>
+              <Transition
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute cursor-pointer z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-custom-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {data.map((element) => (
+                    <Listbox.Option
+                      key={element.id}
+                      className="relative cursor-default select-none py-2 pl-3 pr-9"
+                      value={element}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <div className="flex items-center cursor-pointer">
+                            <span
+                              className={clsx(selected ? 'text-custom-purple' : 'text-custom-black', 'block truncate')}
+                            >
+                              {StringUtils.capitalize(element.id)} {selected && "(Selected)"}
+                            </span>
+                            {selected ? (
+                            <span
+                              className={clsx(
+                                active ? 'text-white' : 'text-custom-purple',
+                                'absolute inset-y-0 right-0 flex items-center pr-4'
+                              )}
+                            >
+                              <></>
+                            </span>
+                          ) : null}
+                          </div>
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </>
+        )}
+      </Listbox>
+    </div>
   )
 }
 
