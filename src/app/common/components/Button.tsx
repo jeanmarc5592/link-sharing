630c543d5d1a7b2a538b09316254e60b811eb219
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-17 13:36:58 +0100

    Add variant styles to Loading Spinner

--- src/app/common/components/Button.tsx
+++ src/app/common/components/Button.tsx
@@ -1,17 +1,16 @@
 import clsx from "clsx";
 import { ButtonHTMLAttributes } from "react"
 import LoadingSpinner from "./LoadingSpinner";
-
-type ButtonVariants = "primary" | "secondary";
+import { ComponentVariants } from "./types";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: ButtonVariants;
+  variant?: ComponentVariants;
   isLoading?: boolean;
   className?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, variant = "primary", isLoading = false, className = "", ...restProps }) => {
-  const variantStyle: { [key in ButtonVariants]: string } = {
+  const variantStyle: { [key in ComponentVariants]: string } = {
     primary: "bg-custom-purple text-white disabled:bg-custom-purple-light active:bg-custom-purple-lighter",
     secondary: "border border-custom-purple text-custom-purple disabled:bg-white disabled:opacity-40 active:bg-custom-purple-light"
   }
@@ -21,7 +20,7 @@ const Button: React.FC<ButtonProps> = ({ children, variant = "primary", isLoadin
       className={clsx("py-3 px-6 rounded-md transition-all w-full sm:px-12", variantStyle[variant], className)}
       {...restProps}
     >
-      {isLoading ? <LoadingSpinner /> : children}
+      {isLoading ? <LoadingSpinner variant={variant} /> : children}
     </button>
   );
 }
