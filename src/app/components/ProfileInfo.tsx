31b47b76d068ea91221a9ebcb367c7d6fb006203
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-21 11:52:52 +0100

    Adjust spacings

--- src/app/components/ProfileInfo.tsx
+++ src/app/components/ProfileInfo.tsx
@@ -13,35 +13,33 @@ const ProfileInfo = () => {
     <FormProvider {...methods}>
       <form className="grid grid-cols-1 bg-custom-gray-light rounded-md p-6 mb-6 sm:grid-cols-3">
         <div className="flex items-center col-span-1">
-          <Typography>First name*</Typography>
+          <Typography>First name</Typography>
         </div>
-        <div className="col-span-2 mb-2">
+        <div className="col-span-2 mb-4 sm:mb-6">
           <Input 
             validationName="firstName"
             noGutter
-            inputProps={{ required: true }}
           />
         </div>
 
         <div className="flex items-center col-span-1">
-          <Typography>Last name*</Typography>
+          <Typography>Last name</Typography>
         </div>
-        <div className="col-span-2 mb-2">
+        <div className="col-span-2 mb-4 sm:mb-6">
           <Input 
             validationName="firstName"
             noGutter
-            inputProps={{ required: true }}
           />
         </div>
 
         <div className="flex items-center col-span-1">
-          <Typography>Email*</Typography>
+          <Typography>Email</Typography>
         </div>
         <div className="col-span-2">
           <Input 
             validationName="firstName"
             noGutter
-            inputProps={{ type: "email", required: true }}
+            inputProps={{ type: "email" }}
           />
         </div>
       </form>
