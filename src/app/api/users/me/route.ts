0cec29f409605c4e0fc2216d6f1673b1a817fc24
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-22 10:39:24 +0100

    Create backend logic to fetch user profile

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,32 @@
+import { AuthService } from '@/server/services/auth';
+import { HttpService } from '@/server/services/http';
+import { UsersService } from '@/server/services/users';
+import { NextRequestWithAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+
+export const GET = async (req: NextRequestWithAuth) => {
+  const httpService = new HttpService();
+  const authService = new AuthService();
+  const usersService = new UsersService();
+
+  const isRequestValid = await authService.validateRequest(req);
+
+  if (isRequestValid !== "OK") {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const token = await httpService.extractToken(req);
+  const userId = await httpService.getUserIdFromToken(token);
+
+  if (!userId) {
+    return NextResponse.json({ message: 'No "sub" in token' }, { status: 400 });
+  }
+
+  const user = await usersService.findById(userId);
+
+  if (!user) {
+    return NextResponse.json({ message: 'Something went wrong reading user' }, { status: 500 });
+  }
+
+  return NextResponse.json({ ...user });
+};
--- src/server/services/users.ts
+++ src/server/services/users.ts
@@ -2,6 +2,24 @@ import prisma from "@/lib/db/prisma";
 import { User } from "@prisma/client";
 
 export class UsersService {
+  async findById(id: string): Promise<Partial<User> | null> {
+    try {
+      const user = await prisma.user.findFirst({
+        where: { id }
+      });
+
+      if (!user) {
+        console.log(`User with id "${id}" not found.`)
+        return null;
+      }
+
+      return this.stripSensitiveData(user);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     try {
       const user = await prisma.user.findUnique({
@@ -32,5 +50,11 @@ export class UsersService {
       console.error(error);
       return null;
     }
+  }
+
+  private stripSensitiveData(user: User): Partial<User> {
+    const { password, ...restUser } = user;
+    
+    return restUser;
   }
 }
