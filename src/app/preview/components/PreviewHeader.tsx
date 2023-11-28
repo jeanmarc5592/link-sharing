c826ac7326cd59d3dcf05eb1f238ccf14f7f752b
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-28 09:37:25 +0100

    Add markup to PreviewHeader

--- src/app/preview/components/PreviewHeader.tsx
+++ src/app/preview/components/PreviewHeader.tsx
@@ -1,7 +1,35 @@
+"use client"
+
+import Button from "@/app/common/components/Button"
+import { ROUTES } from "@/lib/constants/routes";
+import { useRouter } from "next/navigation"
+
 const PreviewHeader = () => {
+  const router = useRouter();
+
+  const navigateToEditor = () => {
+    router.push(ROUTES.home.href);
+  }
+
+  const createShareLink = () => {
+    // TODO: Implement logic to create a share link
+    // TODO: Copy to clipboard automatically
+    // TODO: Render success notification when copied successfully
+  }
+
   return (
-    <div>
-      Preview Header
+    <div className="bg-white rounded-md p-4 flex items-center justify-between">
+      <div>
+        <Button variant="secondary" onClick={navigateToEditor}>
+          Back to Editor
+        </Button>
+      </div>
+
+      <div>
+        <Button onClick={createShareLink}>
+          Share Link
+        </Button>
+      </div>
     </div>
   )
 }
