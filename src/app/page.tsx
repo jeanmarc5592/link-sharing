43e9a6f8ce702126b7dc6890712de3af07d83f7b
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-30 09:02:13 +0100

    Use "isProtected" flag in HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import LogoutButton from "./auth/components/LogoutButton";
-import { authOptions } from "@/lib/auth/options";
 import Header from "./common/components/Header";
 import HomePageContent from "./components/HomePageContent";
 import Page from "./common/components/Page";
-import { ROUTES } from "@/lib/constants/routes";
 
 const HomePage = async () => {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    redirect(ROUTES.auth.login.href);
-  }
-  
   return (
-    <Page>
+    <Page isProtected>
       <Header />
       <HomePageContent />
       <LogoutButton />
