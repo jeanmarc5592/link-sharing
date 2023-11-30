fa6c4acb325ec4b6b1d8aeb8d827856b5be5bad5
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-11-30 08:59:41 +0100

    Do session handling inside page component

--- src/app/common/components/Page.tsx
+++ src/app/common/components/Page.tsx
@@ -1,11 +1,22 @@
+import { authOptions } from "@/lib/auth/options";
+import { ROUTES } from "@/lib/constants/routes";
 import clsx from "clsx";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react"
 
 interface PageProps extends PropsWithChildren {
   hasBackdrop?: boolean;
+  isProtected?: boolean;
 }
 
-const Page: React.FC<PageProps> = ({ children, hasBackdrop }) => {
+const Page: React.FC<PageProps> = async ({ children, hasBackdrop, isProtected = false }) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session && isProtected) {
+    redirect(ROUTES.auth.login.href);
+  }
+
   const backdropStyles = clsx(hasBackdrop ? "bg-transparent sm:bg-custom-purple rounded-b-3xl h-[25vh]" : "")
 
   return (
