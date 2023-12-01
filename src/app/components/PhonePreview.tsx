43895438352c63604f3c2c919debd2a58b208578
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-12-01 09:55:47 +0100

    Add Profile Image

--- src/app/components/PhonePreview.tsx
+++ src/app/components/PhonePreview.tsx
@@ -3,13 +3,19 @@
 import LinkButton from '../common/components/LinkButton';
 import PhoneMockup from '../common/components/icons/PhoneMockup'
 import { useAppSelector } from '../common/hooks/useAppSelector'
+import ProfileCardImage from '../share/components/ProfileCardImage';
 
 const PhonePreview = () => {
   const links = useAppSelector((state) => state.links.list);
+  const profile = useAppSelector((state) => state.profile);
 
   return (
     <div className="relative">
       <PhoneMockup />
+
+      <div className="absolute left-1/2 top-[15%] -translate-x-1/2">
+        <ProfileCardImage picture={profile.picture || undefined} />
+      </div>
 
       <div className="w-[80%] absolute left-1/2 top-[50%] -translate-x-1/2">
         {links?.slice(0, 3).map((link) => {
