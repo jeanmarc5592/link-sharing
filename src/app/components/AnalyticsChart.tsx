c6ff7cf44fbf242ad4198a772ab819993ca4dd6d
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-12-13 09:19:25 +0100

    Adjust sizing of AnalyticsChart

--- src/app/components/AnalyticsChart.tsx
+++ src/app/components/AnalyticsChart.tsx
@@ -1,6 +1,5 @@
-import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Line } from "recharts"
+import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Line, ResponsiveContainer } from "recharts"
 import Typography from "../common/components/Typography"
-import { Platform } from "@prisma/client"
 import { StringUtils } from "@/lib/utils/string";
 import { AnalyticsData } from "./types";
 
@@ -15,13 +14,15 @@ const AnalyticsChart: React.FC<AnalyticsChartProps> = ({ data }) => {
     <>
       <Typography variant="Heading S" className="mb-4">{label}</Typography>
 
-      <LineChart width={650} height={250} data={data.analytics} margin={{ left: -25, right: 16 }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Line type="monotone" dataKey="clicks" stroke="#633CFF" />
-      </LineChart> 
+      <ResponsiveContainer width="90%" aspect={2}>
+        <LineChart height={250} data={data.analytics} margin={{ left: -25, right: 16 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis type="number" domain={[0, 400]} allowDataOverflow />
+          <Tooltip />
+          <Line type="monotone" dataKey="clicks" stroke="#633CFF" />
+        </LineChart>  
+      </ResponsiveContainer> 
     </>
   )
 }
